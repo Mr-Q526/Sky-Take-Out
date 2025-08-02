745f9ad5066249e8698c5ff3c1fb12a9e8b19aa5
MrQ <minruiqing@qq.com>
2025-08-02 14:27:59 +0800

    根据分类id查询菜品

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,6 +2,7 @@ package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
@@ -61,5 +62,17 @@ public class DishController {
         log.info("修改菜品：{}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
         return Result.success();
+    }
+
+    /*
+        根据分类id查询菜品
+        返回 Dish类型， 因为新增套餐页面不关心菜品的口味，只需要添加这个菜品即可
+     */
+    @GetMapping("/list")
+    @ApiOperation("根据分类id查询菜品")
+    public Result<List<Dish>> list(Long categoryId) {
+        List<Dish> list = dishService.list(categoryId);
+        log.info("根据分类id查询菜品：{}", categoryId);
+        return Result.success(list);
     }
 }
