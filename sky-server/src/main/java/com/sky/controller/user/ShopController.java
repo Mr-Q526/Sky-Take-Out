0a49c525f3717ad7cc53821b9644899cb1ca9378
MrQ <minruiqing@qq.com>
2025-08-08 12:55:27 +0800

    商品浏览以及小程序联调

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,30 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import com.sky.service.ShopService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    @Autowired
+    private ShopService shopService;
+
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getSatus(){
+        log.info("获取店铺营业状态");
+        Integer status =shopService.getStatus();
+        return Result.success(status);
+    }
+
+
+
+}
